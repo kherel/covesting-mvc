aa37d164acc5e08a4a9037d1ae682b6df3f48434
Alexandr Adamant <alexandradamant@gmail.com>
2017-11-14 15:02:29 +0300

    [add] sorting logic to subscriptions list

--- src/components/widgets/O_AllSubscriptions/index.js
+++ src/components/widgets/O_AllSubscriptions/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import * as T from 'prop-types'
 import './style.scss'
 import { cssClassName } from 'utils'
@@ -18,77 +18,142 @@ const list = [
     weight: 15,
     totalReturn: 170000,
     strategy: 'Core and Satellite',
+    id: 0,
   },
   {
-    trader: { name: 'Katherine Reyes', imgId: 2 },
+    trader: { name: 'Ralf Hamster', imgId: 2 },
     weight: 50,
     totalReturn: 10000,
     strategy: 'Core and Satellite',
+    id: 1,
   },
   {
-    trader: { name: 'Katherine Reyes', imgId: 2 },
+    trader: { name: 'Glenn Webb', imgId: 2 },
     weight: 20,
     totalReturn: 270000,
     strategy: 'Core and Satellite',
+    id: 2,
   },
   {
-    trader: { name: 'Katherine Reyes', imgId: 2 },
+    trader: { name: 'Jeff Johnson', imgId: 2 },
     weight: 30,
     totalReturn: 15000,
     strategy: 'Core and Satellite',
+    id: 3,
   },
   {
-    trader: { name: 'Katherine Reyes', imgId: 2 },
+    trader: { name: 'Pete Simpson', imgId: 2 },
     weight: 10,
     totalReturn: 6000,
     strategy: 'Core and Satellite',
+    id: 4,
   },
 ]
 
 const cn = cssClassName('O_AllSubscriptions')
 
-const O_AllSubscriptions = ({ mix }) => (
-  <A_Card mix={cn([mix])}>
-    <A_H type="section" mix={cn('title')}>
-      All subscriptions
-    </A_H>
-    <div className={cn('table-header')}>
-      <A_Th mix={cn('th-1')}>Trader</A_Th>
-      <A_ThSortable mix={cn('th-2')}>Total Weight</A_ThSortable>
-      <A_ThSortable mix={cn('th-3')}>Total Return</A_ThSortable>
-      <A_Th mix={cn('th-4')}>Strategy name</A_Th>
-      <A_Th mix={cn('th-5')}>Action</A_Th>
-    </div>
+export default class O_AllSubscriptions extends Component {
+  static propTypes = {
+    mix: T.string,
+  }
 
-    <div className={cn('list')}>
-      {list.map(({ trader: { name, imgId }, weight, totalReturn, strategy }) => (
-        <A_TableRow type="border-blurred" mix={cn('item')}>
-          <div className={cn('th-1')}>
-            <A_Image
-              rounded
-              width={19}
-              height={19}
-              alt={name}
-              mix={cn('logo')}
-              src={`img/userpics/userpic${imgId}.png`}
-            />
-            <A_Span type="table-medium">{name}</A_Span>
-          </div>
-          <A_Span mix={cn('th-2')} type="table-small">{`${weight}%`}</A_Span>
-          <A_ColoredUsdValue mix={cn('th-3')} value={totalReturn} />
-          <A_Span type="table-small-bold" mix={cn('th-4')}>
-            {strategy}
-          </A_Span>
-          <A_Button mix={cn('th-5')} type="button-primary">Unfollow</A_Button>
-        </A_TableRow>
-      ))}
-    </div>
-  </A_Card>
-)
+  state = {
+    sortByWeight: 0, // 0 - no filter, 1 - ascending, -1 - descending
+    sortByReturn: 0,
+  }
 
-O_AllSubscriptions.propTypes = {
-  mix: T.string,
-  children: T.any,
+  toggleWeightSort = () => {
+    const { sortByWeight, sortByReturn } = this.state
+    if (sortByWeight >= 0) {
+      this.setState({ sortByWeight: -1 })
+    } else {
+      this.setState({ sortByWeight: 1 })
+    }
+
+    if (sortByReturn !== 0) {
+      this.setState({ sortByReturn: 0 })
+    }
+  }
+
+  toggleReturnSort = () => {
+    const { sortByWeight, sortByReturn } = this.state
+    if (sortByReturn >= 0) {
+      this.setState({ sortByReturn: -1 })
+    } else {
+      this.setState({ sortByReturn: 1 })
+    }
+
+    if (sortByWeight !== 0) {
+      this.setState({ sortByWeight: 0 })
+    }
+  }
+
+  sortList = (a, b) => {
+    const { sortByReturn, sortByWeight } = this.state
+    if (sortByReturn) {
+      return sortByReturn * a.totalReturn > sortByReturn * b.totalReturn
+    } else if (sortByWeight) {
+      return sortByWeight * a.weight > sortByWeight * b.weight
+    } else {
+      return 0
+    }
+  }
+
+  render() {
+    const { mix } = this.props
+    const { sortByReturn, sortByWeight } = this.state
+    const orderedList = list.slice().sort(this.sortList)
+    return (
+      <A_Card mix={cn([mix])}>
+        <A_H type="section" mix={cn('title')}>
+          All subscriptions
+        </A_H>
+        <div className={cn('table-header')}>
+          <A_Th mix={cn('th-1')}>Trader</A_Th>
+          <A_ThSortable
+            onClick={this.toggleWeightSort}
+            descending={sortByWeight < 0}
+            mix={cn('th-2')}
+          >
+            Total Weight
+          </A_ThSortable>
+          <A_ThSortable
+            onClick={this.toggleReturnSort}
+            descending={sortByReturn < 0}
+            mix={cn('th-3')}
+          >
+            Total Return
+          </A_ThSortable>
+          <A_Th mix={cn('th-4')}>Strategy name</A_Th>
+          <A_Th mix={cn('th-5')}>Action</A_Th>
+        </div>
+
+        <div className={cn('list')}>
+          {orderedList.map(({ trader: { name, imgId }, weight, totalReturn, strategy, id }) => (
+            <A_TableRow type="border-blurred" mix={cn('item')} key={id}>
+              <div className={cn('th-1')}>
+                <A_Image
+                  rounded
+                  width={19}
+                  height={19}
+                  alt={name}
+                  mix={cn('logo')}
+                  src={`img/userpics/userpic${imgId}.png`}
+                />
+                <A_Span type="table-medium">{name}</A_Span>
+              </div>
+              <A_Span mix={cn('th-2')} type="table-small">{`${weight}%`}</A_Span>
+              <A_ColoredUsdValue mix={cn('th-3')} value={totalReturn} />
+              <A_Span type="table-small-bold" mix={cn('th-4')}>
+                {strategy}
+              </A_Span>
+              <A_Button mix={cn('th-5')} type="button-primary">
+                Unfollow
+              </A_Button>
+            </A_TableRow>
+          ))}
+        </div>
+      </A_Card>
+    )
+  }
 }
-
-export default O_AllSubscriptions
