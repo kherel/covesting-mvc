3e00f65d13042e2d4acd5b1472e882b37484db22
Niten <niten2@yandex.ru>
2017-11-15 18:23:52 +0700

    add auth

--- src/store/entities/user/actions.js
+++ src/store/entities/user/actions.js
@@ -1,17 +1,15 @@
 import superagent from 'superagent'
 
 const url = process.env.BACKEND_URL
-console.log(url)
 
 export const createUser = async () => {
 
   let res = await superagent
     .get(`${url}/users`)
     .set('Accept', 'application/json')
-  // let res = await superagent.get(`${url}/users`)
+
+
   console.log(res)
-
-
 
 }
 
---
+++ src/utils/auth_provider.js
@@ -0,0 +1,45 @@
+// import authProvider from "lib/auth_provider"
+
+const AUTH_SESSION_STORAGE_KEY = "covesting-mvc"
+
+class AuthProvider {
+
+  constructor() {
+    this.storage = this._supportsHtml5Storage() ? localStorage : null
+  }
+
+  _supportsHtml5Storage() {
+    try {
+      return 'localStorage' in window && window['localStorage'] !== null
+    } catch (e) {
+      return false
+    }
+  }
+
+  fetchToken() {
+    const token = this.storage && this.storage.getItem(AUTH_SESSION_STORAGE_KEY)
+    return `Bearer ${token}`
+  }
+
+  token() {
+    const token = this.storage && this.storage.getItem(AUTH_SESSION_STORAGE_KEY)
+    return token
+  }
+
+  saveToken(token) {
+    if (!this.storage || !token) { return null }
+    this.storage.setItem(AUTH_SESSION_STORAGE_KEY, token)
+  }
+
+  removeToken() {
+    if (!this.storage) return null
+    this.storage.removeItem(AUTH_SESSION_STORAGE_KEY)
+  }
+
+  hasLogin() {
+    return this.token() != null
+  }
+
+}
+
+export default new AuthProvider()
